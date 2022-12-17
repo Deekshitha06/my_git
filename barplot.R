1f8960c878a2b2dcf28bc6db1a6dbbe6c9733bd4
P Deekshitha <peddireddideekshita@gmail.com>
2022-12-17 08:13:15 +0530

    modify

---
+++ barplot.R
@@ -0,0 +1,9 @@
+marks=c(50,67,56,90);
+barplot(marks,
+main="comparing marks of 4 subjects",
+xlab="marks",
+ylab="subjects",
+names.arg=c("english","maths","chemistry","biology"),
+col="blue",
+horiz=FALSE)
+
